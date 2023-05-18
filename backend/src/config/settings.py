6062aafbd4dc69b8d5ce77572ca21f9d9fb348ec
AlexandrZhydyk <zhydykalex@ukr.net>
2023-05-18 18:43:19 +0300

    feat: added product app with db models

--- backend/src/config/settings.py
+++ backend/src/config/settings.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,6 +26,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
+    "products"
 ]
 
 MIDDLEWARE = [
@@ -109,3 +112,6 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+MEDIA_URL = "media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")

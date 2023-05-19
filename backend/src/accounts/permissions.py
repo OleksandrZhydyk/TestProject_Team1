d5b0db410b3f121cc64f34ca116fcd96d4f94c8d
mak-gara <maksimgaragulia@gmail.com>
2023-05-19 07:50:19 +0300

    refactor: change access to registration, only unauthorized users can register

--- backend/src/accounts/permissions.py
+++ backend/src/accounts/permissions.py
@@ -1,6 +1,13 @@
 from rest_framework.permissions import BasePermission
 
 
+class IsUnauthenticated(BasePermission):
+    def has_permission(self, request, view):
+        if request.user.is_anonymous:
+            return True
+        return False
+
+
 class IsOwnerOrStaff(BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.user.is_staff:
--- backend/src/accounts/views.py
+++ backend/src/accounts/views.py
@@ -1,19 +1,18 @@
 from django.contrib.auth.models import User
 
-from rest_framework.generics import RetrieveUpdateAPIView
 from rest_framework.views import APIView
-from rest_framework.permissions import AllowAny
+from rest_framework.generics import RetrieveUpdateAPIView
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.response import Response
 
 from .serializers import UserSerializer
-from .permissions import IsOwnerOrStaff
+from .permissions import IsUnauthenticated, IsOwnerOrStaff
 
 
 class UserRegistrationAPIView(APIView):
-    authentication_classes = [JWTAuthentication]
-    permission_classes = [AllowAny]
+    authentication_classes = (JWTAuthentication,)
+    permission_classes = (IsUnauthenticated,)
 
     def post(self, request):
         serializer = UserSerializer(data=request.data)

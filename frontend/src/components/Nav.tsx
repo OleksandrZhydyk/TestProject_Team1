3a26b00ae8d748667aa2b5b355219345ef9d9829
KirillSerg <kirill.sergienko1989@gmail.com>
2023-05-20 21:48:44 +0300

    add materialUI dependencies; start login form

--- frontend/src/components/Nav.tsx
+++ frontend/src/components/Nav.tsx
@@ -1,5 +1,11 @@
 import styled from "styled-components";
 
+import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
+import { IconButton } from "@mui/material";
+
+import LoginForm from "./LoginForm";
+import { useState } from "react";
+
 const StyledNav = styled.nav`
   display: flex;
   align-items: centr;
@@ -9,9 +15,19 @@ const StyledNav = styled.nav`
 `;
 
 const Nav = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false)
+
+  const handleClickOpen = () => {
+    setIsOpen(true);
+  };
+
   return (
     <StyledNav>
       Nav
+      <IconButton onClick={handleClickOpen} aria-label="login">
+        <PersonOutlineOutlinedIcon />
+      </IconButton>
+      <LoginForm isOpen={isOpen} setIsOpen={setIsOpen} />
     </StyledNav>
   );
 };
